155d74b44036aabdce65282336f91430b3806c8c
GersonRS <gersonrs@live.com>
2021-10-21 19:21:18 -0300

    feat(add tests): adding a test class and updating package.json

---
+++ __tests__/src/screens/home.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Home from '../../../src/screens/Home';
+
+describe('<Home />', () => {
+  it('has 1 child', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree.children.length).toBe(1);
+  });
+});
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,13 +2,6 @@ import React from 'react';
 import InputText from '../../components/InputText';
 import { Container, Text } from './styles';
 
-export const addBookmark = book => dispatch => {
-  dispatch({
-    type: ADD_TO_BOOKMARK_LIST,
-    payload: book
-  });
-};
-
 const Home = () => {
   return(
     <Container>
